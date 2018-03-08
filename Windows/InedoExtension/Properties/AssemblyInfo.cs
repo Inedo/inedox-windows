e8a28d86ff61feeb64f3602a39c6a4056f1a96b7
Greg D <gdivis@inedo.com>
2018-03-08 15:08:38 -0500

    Removed legacy extension

--- Windows/InedoExtension/Properties/AssemblyInfo.cs
+++ Windows/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,3 +1,14 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyProduct("Hedgehog")]
+[assembly: AssemblyTitle("Windows")]
+[assembly: AssemblyDescription("Contains operations to interact with Windows features, including IIS and services.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyProduct("any")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2018")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: CLSCompliant(false)]
