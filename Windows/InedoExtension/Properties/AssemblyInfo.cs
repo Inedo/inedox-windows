7832099bf53e9fc3e9f483f7699e01127f3f079f
Richard Hessinger <rhessinger@inedo.com>
2020-11-19 22:46:53 -0500

    updated copyright year

--- Windows/InedoExtension/Properties/AssemblyInfo.cs
+++ Windows/InedoExtension/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using Inedo.Extensibility;
 [assembly: AssemblyDescription("Contains operations to interact with Windows features, including IIS and services.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("any")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2020")]
 [assembly: AssemblyVersion("1.9.0")]
 [assembly: AssemblyFileVersion("1.9.0")]
 [assembly: CLSCompliant(false)]
