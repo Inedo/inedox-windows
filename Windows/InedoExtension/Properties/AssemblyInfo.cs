ddea9b785b2468955e11a8c25a7118d97b2a2621
Richard Hessinger <rhessinger@inedo.com>
2020-11-19 22:46:20 -0500

    Updated assembly version to 1.9.0

--- Windows/InedoExtension/Properties/AssemblyInfo.cs
+++ Windows/InedoExtension/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using Inedo.Extensibility;
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("any")]
 [assembly: AssemblyCopyright("Copyright © Inedo 2018")]
-[assembly: AssemblyVersion("1.0.8")]
-[assembly: AssemblyFileVersion("1.0.8")]
+[assembly: AssemblyVersion("1.9.0")]
+[assembly: AssemblyFileVersion("1.9.0")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 
