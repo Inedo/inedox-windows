32ca5365dd3665d213ef13acf4f23991120dc6e6
Greg Divis <gdivis@inedo.com>
2022-03-23 12:05:09 -0400

    sdk 2.0 support

--- Windows/InedoExtension/Properties/AssemblyInfo.cs
+++ Windows/InedoExtension/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using Inedo.Extensibility;
 [assembly: AssemblyDescription("Contains operations to interact with Windows features, including IIS and services.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("any")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2020")]
-[assembly: AssemblyVersion("1.9.0")]
-[assembly: AssemblyFileVersion("1.9.0")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2022")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 
