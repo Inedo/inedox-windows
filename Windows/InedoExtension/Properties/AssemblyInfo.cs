42067a39a2a193e607ae80600ac0950d3b7fdd4c
Greg Divis <gdivis@inedo.com>
2024-10-17 14:52:04 -0400

    update to sdk 3.1

--- Windows/InedoExtension/Properties/AssemblyInfo.cs
+++ Windows/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using Inedo.Extensibility;
 
@@ -7,9 +6,9 @@ using Inedo.Extensibility;
 [assembly: AssemblyDescription("Contains operations to interact with Windows features, including IIS and services.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("any")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2022")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2024")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 
