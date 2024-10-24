da70e6ff2e17b951f9b363f05374b23e2f7eaf77
Richard Hessinger <rhessinger@inedo.com>
2024-10-24 17:39:40 -0400

    Refactor EnsureRegistryValueOperation to work as:
    Windows::Ensure-RegistryKeyValue
    (
        Path: HKLM:SOFTWARE\Inedo\BuildMaster,
        Name: ServicePath,
        Value: C:\BuildMaster\Service,
    );

--- Windows/InedoExtension/Configurations/Registry/RegistryConfiguration.cs
+++ Windows/InedoExtension/Configurations/Registry/RegistryConfiguration.cs
@@ -4,36 +4,38 @@ using Inedo.Extensibility;
 using Inedo.Extensibility.Configurations;
 using Inedo.Serialization;
 
-namespace Inedo.Extensions.Windows.Configurations.Registry
+namespace Inedo.Extensions.Windows.Configurations.Registry;
+
+[Serializable]
+public abstract class RegistryConfiguration : PersistedConfiguration, IExistential
 {
-    [Serializable]
-    public abstract class RegistryConfiguration : PersistedConfiguration, IExistential
+    private protected RegistryConfiguration()
     {
-        private protected RegistryConfiguration()
-        {
-        }
+    }
 
-        [Required]
-        [Persistent]
-        [ConfigurationKey]
-        [ScriptAlias("Hive")]
-        public InedoRegistryHive Hive { get; set; }
-        [Required]
-        [Persistent]
-        [ConfigurationKey]
-        [ScriptAlias("Key")]
-        public string Key { get; set; }
-        [Persistent]
-        public abstract bool Exists { get; set; }
+    [Undisclosed]
+    [Persistent]
+    [ScriptAlias("Hive")]
+    public InedoRegistryHive Hive { get; set; }
+    [Undisclosed]
+    [Persistent]
+    [ScriptAlias("Key")]
+    public string Key { get; set; }
+    [Persistent]
+    [Required]
+    [ConfigurationKey]
+    [ScriptAlias("Path")]
+    public string Path { get; set; }
+    [Persistent]
+    public abstract bool Exists { get; set; }
 
-        public string GetDisplayPath() => this.Hive.GetAbbreviation() + "\\" + GetCanonicalKey(this.Key);
+    public string GetDisplayPath() => this.Path ?? (this.Hive.GetAbbreviation() + ":" + GetCanonicalKey(this.Key));
 
-        public static string GetCanonicalKey(string key)
-        {
-            if (string.IsNullOrEmpty(key))
-                return string.Empty;
+    public static string GetCanonicalKey(string key)
+    {
+        if (string.IsNullOrEmpty(key))
+            return string.Empty;
 
-            return string.Join("\\", key.Split(new[] { '\\', '/' }, StringSplitOptions.RemoveEmptyEntries));
-        }
+        return string.Join("\\", key.Split(new[] { '\\', '/' }, StringSplitOptions.RemoveEmptyEntries));
     }
 }
--- Windows/InedoExtension/InternalExtensions.cs
+++ Windows/InedoExtension/InternalExtensions.cs
@@ -1,19 +1,31 @@
-﻿namespace Inedo.Extensions.Windows
+﻿namespace Inedo.Extensions.Windows;
+
+internal static class InternalExtensions
 {
-    internal static class InternalExtensions
+    public static string GetAbbreviation(this InedoRegistryHive hive)
     {
-        public static string GetAbbreviation(this InedoRegistryHive hive)
+        return hive switch
         {
-            return hive switch
-            {
-                InedoRegistryHive.ClassesRoot => "HKCR",
-                InedoRegistryHive.CurrentUser => "HKCU",
-                InedoRegistryHive.LocalMachine => "HKLM",
-                InedoRegistryHive.Users => "HKU",
-                InedoRegistryHive.CurrentConfig => "HKCC",
-                InedoRegistryHive.PerformanceData => "HKPD",
-                _ => "(unknown)"
-            };
-        }
+            InedoRegistryHive.ClassesRoot => "HKCR",
+            InedoRegistryHive.CurrentUser => "HKCU",
+            InedoRegistryHive.LocalMachine => "HKLM",
+            InedoRegistryHive.Users => "HKU",
+            InedoRegistryHive.CurrentConfig => "HKCC",
+            InedoRegistryHive.PerformanceData => "HKPD",
+            _ => "(unknown)"
+        };
+    }
+    public static InedoRegistryHive GetInedoHiveRegistry(this string hive)
+    {
+        return hive switch
+        {
+            "HKCR" => InedoRegistryHive.ClassesRoot,
+            "HKCU" => InedoRegistryHive.CurrentUser,
+            "HKLM" => InedoRegistryHive.LocalMachine,
+            "HKU" => InedoRegistryHive.Users,
+            "HKCC" => InedoRegistryHive.CurrentConfig,
+            "HKPD" => InedoRegistryHive.PerformanceData,
+            _ => throw new InvalidOperationException($"Cannot identify hive {hive}")
+        };
     }
 }
